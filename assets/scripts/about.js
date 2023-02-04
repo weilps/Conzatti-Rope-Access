2138187763f966c2b0b09ac26715b3ee960c5fb6
weilps <guillaume.gaudin75@gmail.com>
2023-02-04 18:11:23 +1100

    v0.2
    CSS architecture 40%

---
+++ assets/scripts/about.js
@@ -0,0 +1,31 @@
+// Script for hovering the team
+
+const loicT = document.getElementById("loic");
+const amauryT = document.getElementById("amaury");
+const techniciansT = document.getElementById("technicians");
+
+const noneD = document.getElementById("none");
+const loicD = document.getElementById("Loic");
+const amauryD = document.getElementById("Amaury");
+const techniciansD = document.getElementById("Technicians");
+
+loicT.addEventListener("mouseover", function () {
+  noneD.style.display = "none";
+  loicD.style.display = "block";
+  amauryD.style.display = "none";
+  techniciansD.style.display = "none";
+});
+
+amauryT.addEventListener("mouseout", function () {
+  noneD.style.display = "none";
+  loicD.style.display = "none";
+  amauryD.style.display = "block";
+  techniciansD.style.display = "none";
+});
+
+techniciansT.addEventListener("mouseout", function () {
+  noneD.style.display = "none";
+  loicD.style.display = "none";
+  amauryD.style.display = "none";
+  techniciansD.style.display = "block";
+});
