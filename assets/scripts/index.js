d6b49426f065f21a859a2ab74b571e22696cf952
weilps <guillaume.gaudin75@gmail.com>
2023-03-05 11:43:12 +1100

    v1.1 - mobile
    mobile and tablet responsive

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,13 +3,9 @@ const bBButton = document.querySelector(".burger-bun");
 const menu = document.querySelector(".menu");
 const socialsH = document.getElementById("soc-header");
 
-console.log(bBButton);
-console.log(menu);
-console.log(socialsH);
 // Add a click event listener to the button
 bBButton.addEventListener("click", () => {
   // Toggle the 'open' class on the nav menu
-  console.log("CLICK !!");
   menu.classList.toggle("open");
   socialsH.classList.toggle("open");
 });
